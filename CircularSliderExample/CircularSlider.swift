3e12a6fda4919c23bde9a68e66179a2a286d91ac
chungdan <chungdan@coupang.com>
2017-06-04 22:26:44 +0900

    first commit

---
+++ CircularSliderExample/CircularSlider.swift
@@ -0,0 +1,228 @@
+//
+//  CircularSlider.swift
+//  CircularSlider
+//
+//  Created by Coupang on 2017. 5. 19..
+//  Copyright © 2017년 Coupang. All rights reserved.
+//
+
+import UIKit
+
+enum SliderHandleType {
+    case transparentWhiteColor
+}
+
+@IBDesignable
+class CircularSlider: UIControl {
+    
+    @IBInspectable open var minimumValue: Double = 0.0
+    @IBInspectable open var maximumValue: Double = 100.0
+    @IBInspectable open var currentValue: Double = 0.0 {
+        didSet {
+            if currentValue > maximumValue {
+                currentValue = maximumValue
+            } else if currentValue < minimumValue {
+                currentValue = minimumValue
+            }
+            
+            self.setNeedsLayout()
+            self.setNeedsDisplay()
+            self.sendActions(for: .valueChanged)
+        }
+    }
+    
+    @IBInspectable open var handleRadius: CGFloat = 0.0 {
+        didSet {
+            if handleRadius * 2 < sliderLineWidth {
+                handleRadius = sliderLineWidth / 2.0
+            }
+        }
+    }
+    
+    fileprivate var fixedAngle: Double = 0.0
+    fileprivate var centerPoint: CGPoint {
+        get {
+            return CGPoint(x: self.frame.size.width / 2.0, y: self.frame.size.height / 2.0)
+        }
+    }
+    
+    fileprivate var sliderLineWidth: CGFloat = 1.0
+    fileprivate var sliderLineRadiusDisplayment: CGFloat = 0.0
+    fileprivate var unfilledColor: UIColor = UIColor.black
+    fileprivate var filledColor: UIColor = UIColor.white
+    
+    fileprivate var angle: Double {
+        get {
+            let angle = 360.0 - (360.0 * currentValue / maximumValue)
+            return angle == 360.0 ? 0.0 : angle
+        }
+    }
+    
+    fileprivate var circleDiameter: CGFloat {
+        get {
+            switch self.handleType {
+            case .transparentWhiteColor:
+                return self.sliderLineWidth
+            }
+        }
+    }
+    
+    fileprivate var circleRadius: CGFloat = 0.0
+    
+    @IBInspectable open var radius: CGFloat {
+        set {
+            circleRadius = newValue > self.frame.size.height / 2.0 - self.sliderLineWidth / 2.0 - (self.circleDiameter - self.sliderLineWidth) - self.sliderLineRadiusDisplayment ? self.frame.size.height / 2.0 - self.sliderLineWidth / 2.0 - (self.circleDiameter - self.sliderLineWidth) - self.sliderLineRadiusDisplayment : newValue
+        }
+        get {
+            return circleRadius
+        }
+    }
+    
+    open var handleType: SliderHandleType = .transparentWhiteColor
+    
+    @IBInspectable open var handleColor: UIColor = UIColor.white
+    
+    @IBInspectable open var lineWidth: CGFloat {
+        set {
+            sliderLineWidth = newValue
+            setNeedsDisplay()
+        }
+        get {
+            return sliderLineWidth
+        }
+    }
+    
+    @IBInspectable open var sliderBackgroundColor: UIColor {
+        set {
+            unfilledColor = newValue
+            setNeedsDisplay()
+        }
+        get {
+            return unfilledColor
+        }
+    }
+    
+    @IBInspectable open var sliderColor: UIColor {
+        set {
+            filledColor = newValue
+            setNeedsDisplay()
+        }
+        get {
+            return filledColor
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    // MARK: - drawing methods
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        guard let ctx: CGContext =  UIGraphicsGetCurrentContext() else {
+            return
+        }
+        
+        //draw the unfiled circle
+        
+        let center = CGPoint(x: self.frame.size.width/2,y: self.frame.size.height/2)
+        
+        ctx.addRect(CGRect(x: self.bounds.origin.x, y: self.bounds.origin.y, width: self.bounds.size.width, height: self.bounds.size.height))
+        UIColor.clear.set()
+        ctx.setLineCap(.butt)
+        ctx.drawPath(using: .fillStroke)
+        
+        
+        ctx.addArc(center: center, radius: self.radius, startAngle: 0.0, endAngle: CGFloat(Double.pi * 2.0), clockwise: false)
+        self.unfilledColor.setStroke()
+        ctx.setLineWidth(sliderLineWidth)
+        ctx.setLineCap(.butt)
+        ctx.drawPath(using: .stroke)
+        
+        ctx.addArc(center: center, radius: self.radius, startAngle: CGFloat(3 * Double.pi / 2.0), endAngle: CGFloat(3 * Double.pi / 2.0 - self.angle.degreesToRadians), clockwise: false)
+        self.filledColor.setStroke()
+        ctx.setLineWidth(sliderLineWidth)
+        ctx.setLineCap(.butt)
+        ctx.drawPath(using: .stroke)
+
+        self.drawHandle(ctx)
+    }
+
+    fileprivate func drawHandle(_ ctx: CGContext) {
+        ctx.saveGState()
+        let handleCenter: CGPoint = self.pointFromCurrentAngle()
+        self.handleColor.set()
+        ctx.fillEllipse(in: CGRect(x: handleCenter.x - (handleRadius * 2.0 - sliderLineWidth) / 2.0, y: handleCenter.y - (handleRadius * 2.0 - sliderLineWidth) / 2.0, width: handleRadius * 2.0, height: handleRadius * 2.0))
+        ctx.restoreGState()
+    }
+    
+    fileprivate func pointFromCurrentAngle() -> CGPoint {
+        let centerPoint = CGPoint(x: self.frame.size.width / 2.0 - sliderLineWidth / 2.0, y: self.frame.size.height / 2.0 - sliderLineWidth / 2.0)
+        
+        return CGPoint(x: centerPoint.x + radius * CGFloat(cos((-angle-90).degreesToRadians)), y: centerPoint.y + radius * CGFloat(sin((-angle-90).degreesToRadians)))
+    }
+    
+    override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        return self.bounds.contains(point);
+    }
+    
+    // MARK: - UIControl methods
+    
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        super.beginTracking(touch, with: event)
+        
+        return true
+    }
+    
+    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        super.continueTracking(touch, with: event)
+        let lastPoint = touch.location(in: self)
+        self.moveHandle(lastPoint)
+        self.sendActions(for: .valueChanged)
+        
+        return true
+    }
+    
+    
+    fileprivate func moveHandle(_ point:CGPoint) {
+        let currentAngle = floor(angleFromNorth(centerPoint, point, false))
+        print(currentAngle)
+        currentValue = self.valueFrom(currentAngle)
+        if currentAngle >= 359.0 {
+            
+        }
+        
+        self.setNeedsDisplay()
+    }
+    
+    fileprivate func valueFrom(_ angle: Double) -> Double {
+        return angle * (maximumValue - minimumValue) / 360
+    }
+    
+    fileprivate func angleFromNorth(_ fromPoint:CGPoint,_ toPoint:CGPoint,_ flipped: Bool) -> Double {
+        var v: CGPoint = CGPoint(x: toPoint.x - fromPoint.x, y: toPoint.y - fromPoint.y)
+        let vmag: Double = sqrt(Double(v.x * v.x + v.y * v.y))
+        var result: Double = 0.0
+        v.x = v.x / CGFloat(vmag)
+        v.y = v.y / CGFloat(vmag)
+        let radians: Double = atan2(Double(v.y), Double(v.x))
+        result = radians.radiansToDegrees
+        return result >= -90.0 ? result + 90 : result + 450.0
+    }
+    
+}
+
+extension Int {
+    var degreesToRadians: Double { return Double(self) * .pi / 180 }
+}
+
+extension FloatingPoint {
+    var degreesToRadians: Self { return self * .pi / 180 }
+    var radiansToDegrees: Self { return self * 180 / .pi }
+}
---
+++ CircularSliderExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CircularSliderExample
+//
+//  Created by Coupang on 2017. 6. 4..
+//  Copyright © 2017년 Byoungho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
